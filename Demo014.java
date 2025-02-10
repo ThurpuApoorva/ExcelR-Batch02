73bcfdf2bae2c5bc4ce2c8190e077d3df1c5eedd
ThurpuApoorva <apoorvar757@gmail.com>
2025-02-10 19:17:21 +0530

    unary operators

---
+++ Demo014.java
@@ -0,0 +1,10 @@
+public class Demo014 {
+        public static void main(String[] args) {
+            //increment and Drecrement operators
+            int x=1;
+            x++;
+            System.out.println(x);
+        }
+        
+    }
+    
