ef24c2757271c9d069830cbee95b973aaa643832
ThurpuApoorva <apoorvar757@gmail.com>
2025-02-10 19:13:29 +0530

    Username and Password

---
+++ Demo006.java
@@ -0,0 +1,11 @@
+public class Demo006 {
+    public static void main(String[] args){
+        String name="Sravya";
+        String password="sravyareddy@123";
+        while((name=="Sravya")&&(password=="sravyareddy@123")){
+            System.out.println("hello Have A Nice Day");
+            break;
+    }
+    
+}
+}
