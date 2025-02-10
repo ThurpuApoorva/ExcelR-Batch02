263c28dd7be57abffa4efe46bbceecf15f564a45
ThurpuApoorva <apoorvar757@gmail.com>
2025-02-10 19:17:44 +0530

    Swapping numbers

---
+++ Demo015.java
@@ -0,0 +1,18 @@
+public class Demo015 {
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 10;
+
+            System.out.println("Before swapping: a = " + a + ", b = " + b);
+    
+            int temp = a;
+            a = b;
+            b = temp;
+    
+            // Output after swapping
+            System.out.println("After swapping: a = " + a + ", b = " + b);
+        }
+    }
+    
+    
+
