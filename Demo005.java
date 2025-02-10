4fc55d4453e9788718da1b87d6c173e344d32905
ThurpuApoorva <apoorvar757@gmail.com>
2025-02-10 19:13:06 +0530

    password

---
+++ Demo005.java
@@ -0,0 +1,10 @@
+public class Demo005 {
+    public static void main(String[] args){
+    String username = "Yashwanth";
+    String password = "Yashwanth@123";
+    while((username=="Yashwanth") && (password=="Yashwanth@123")){
+    System.out.println("hello Happy new year");
+    break;
+    }
+} 
+}
