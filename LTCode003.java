336d69757bbd29ced11b845214709ccb425118bb
ThurpuApoorva <apoorvar757@gmail.com>
2025-02-12 23:23:06 +0530

    String - Integer

---
+++ LTCode003.java
@@ -0,0 +1,48 @@
+public class LTCode003 {
+    public int myAtoi(String s) {
+        
+        s = s.trim();
+        if (s.isEmpty()) {
+            return 0;
+        }
+        int sign = 1;
+        int i = 0;
+        if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+
+        long result = 0;
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+            i++;
+
+            
+            if (result > Integer.MAX_VALUE) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+        } 
+        result *= sign;
+        if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        return (int) result;
+    }
+
+    
+    public static void main(String[] args) {
+        LTCode003 solution = new LTCode003();
+        System.out.println(solution.myAtoi("42"));            // Output: 42
+        System.out.println(solution.myAtoi("   -042"));       // Output: -42
+        System.out.println(solution.myAtoi("1337c0d3"));      // Output: 1337
+        System.out.println(solution.myAtoi("0-1"));           // Output: 0
+        System.out.println(solution.myAtoi("words and 987")); // Output: 0
+        System.out.println(solution.myAtoi("21474836460"));   // Output: 2147483647
+    }
+}
